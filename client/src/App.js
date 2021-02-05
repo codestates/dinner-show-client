5dfa1f308ae1275ff7687d228d3c2ba5adb97188
JuicyJerry <Ju2cyjerry@gmail.com>
2021-02-05 17:45:03 +0900

    MainPage 1/5

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './components/Nav';
+import Header from './components/Header';
+import Main from './components/Main';
+// import axios from 'axios'; // install 해야함
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    //! For Main component state
+      this.state = {
+        data: props
+    }
+  }
+
+  // getSearchData() {
+  //   axios.get()
+  
+
+
+  render() {
+    return (
+      <div className="container">
+            <Nav />
+            <Header />
+            <Main />
+            <Main />
+            <Main />
+            <Main />
+            <Main />
+            <Main />
+      </div> 
+  
+    );
+  }
+}
+
+export default App;
