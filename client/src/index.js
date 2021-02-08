38b3b4a4951ccc7740a8c98bd7c861bb7b640ba8
tmddlr <a01092813007@gmail.com>
2021-02-08 23:40:37 +0900

    updatefordeploy

--- client/src/index.js
+++ client/src/index.js
@@ -6,15 +6,8 @@ import Mypage from "./MyPageComponents/Mypage";
 import SignupPage from "./SignupPagecomponents/SignupPage";
 import WritePage from "./WritePagecomponents/WritePage";
 
-
-import React from 'react';
-import ReactDOM from 'react-dom';
 // import LoginPage from './LoginPageComponents/LoginPage.js';
-import MainPage from './MainPageComponents/MainPage.js';
+import MainPage from "./MainPageComponents/MainPage.js";
 // import ReadPage from './ReadPageComponents/ReadPage.js';
 
-ReactDOM.render(
-  <MainPage />,
-  document.getElementById('root')
-
-);
+ReactDOM.render(<MainPage />, document.getElementById("root"));
