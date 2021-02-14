975552ade0ca3d80f3c1baa19bb78c3413e3e82a
JuicyJerry <Ju2cyjerry@gmail.com>
2021-02-14 09:30:01 +0900

    make deployment branch

--- client/src/routes/NoMatch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoMatch = () => {
-  return <div>여기는 어떻게 오셨나요.. 404 에러</div>;
-};
-
-export default NoMatch;
--- client/src/routes/Toggle.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const Toggle = () => {
-  return <div>토글</div>;
-};
-
-export default Toggle;
